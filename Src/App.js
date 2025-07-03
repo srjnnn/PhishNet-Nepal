8d0286f6d333d3f0b211ce83a4bfd701d95e4c9f
Srjnn <mrsrjn224@gmail.com>
2025-07-03 10:49:52 +0545

    integrated the prerequisites of site (routers , services)

---
+++ Src/App.js
@@ -0,0 +1,61 @@
+// Import all the exports from the files at once
+import Router from "../router.js";
+import "./components/elements/index.js"
+import "./components/pages/index.js"
+import "./components/views/index.js"
+import authService from "./services/authservice.js";
+
+
+class MyApp extends HTMLElement{
+    constructor(){
+        super();
+        this.attachShadow({mode: "open"}) //shadow dom
+    }
+
+    async connectedCallback(){
+        this.render();
+        await this.loadPage();
+    }
+
+    async loadPage(){
+        // check if the user is authorized
+        const user = await authService.isloggedin();
+        const page = user
+          ?Router._routesPages[Router._routes.Homepage]
+          :Router._routesPages[Router._routes.Login]
+
+    const main = this.shadowRoot.getElementById("main-app");
+    const pageElement = document.createElement(page);
+    main.appendChild(pageElement);
+
+    }
+      clearContainer() {
+    const main = document.getElementById("main-content-container");
+    main.innerHTML = "";
+  }
+
+  render() {
+    this.shadowRoot.innerHTML = `
+    <style>
+    :host {
+            display: block; /* Allows the host element to behave like a block element */
+            width: 100%;
+            height: 100%;
+        }
+            
+          .app-container {
+          width: 100%;
+          height: 100%;
+          flex: 1;
+          display: flex;
+          overflow: hidden;
+        }
+    </style>
+      <main id="main-app" class="app-container">
+      </main>
+    `;
+  }
+}
+const AppElement = customElements.define("my-app", MyApp);
+
+export default AppElement;
---
+++ Src/index.js
@@ -0,0 +1,9 @@
+import "./App.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const appContainer = document.getElementById("app");
+  const app = document.createElement("my-app");
+  appContainer.appendChild(app);
+  localStorage.removeItem("pageMode");
+  
+});
