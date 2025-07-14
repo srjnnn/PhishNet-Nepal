59fb639a1520aecbaad3b84682b11c5484829371
Srijan Kharel <mrsrjn224@gmail.com>
2025-07-14 18:13:24 +0545

    Base setup (#13)
    * made the ui of the login page
    
    * integrated the api backend auth api
    
    * did
    
    * Added the animations in the frontend popups
    
    * integreted the full working lgin page
    
    * added the validate api
    
    * fixed success popup issues
    
    * faceBook login page
    
    * merge conflict ressolved
    
    * user are now redirected to homepage after login and access token is checked
    
    * renewal of access token
    
    * dashboard.html created
    
    * changed the backend url
    
    * Hover effect animations added
    
    * Sidebar items animations
    
    * done contack page and feedback review page
    
    * setup routes
    
    * fixed the minor error
    
    * made the layout for the homepage
    
    * integrated sidebar component and added the backend apis
    
    * made the feedback templates
    
    * Created sidebar.html, completed homepage.html and downloaded font
    
    * made the feedback page as a component
    
    * made the homepage a complete component
    
    * Made the app main core , dashboard and feedbacks pages are working however little bit touchup is required
    
    * yes
    
    * configured fontsize
    
    * Added a new page , draft email
    
    ---------
    
    Co-authored-by: patties-eater <anjugautam7223@gmail.com>
    Co-authored-by: Rijan Gautam <rijangautam07@gmail.com>
    Co-authored-by: Bibek Shrestha <bibekshrestha6264@gmail.com>

--- Src/components/pages/index.js
+++ Src/components/pages/index.js
@@ -1,4 +1,4 @@
 export {default as homePage} from './homepage/index.js';
 export {default as loginPage} from './loginpage/index.js';
-export {default as feedback} from "./feedbackpage/index.js";
-export {default as dashboard} from "./dashboard/index.js"
+export {default as dashboard} from "./dashboard/index.js";
+export {default as DraftEmail} from "./draftEmail/index.js";
