bd23a3e851405e06e3cce3a3bff7551d9975c478
Srjnn <mrsrjn224@gmail.com>
2025-07-15 10:51:08 +0545

    made two pages fully dynamic

--- Src/components/pages/clients/index.js
+++ Src/components/pages/clients/index.js
@@ -1,26 +1,69 @@
 import { apiRoutes } from "../../../globalConstants.js";
 import apiRequest from "../../../utils/api.js";
 import { loadTemplate } from "../../../utils/loadtemplate.js";
+import Common from "../../../utils/common.js"; 
 
+class clients extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.templateContent = null;
+    this.data = null;
+  }
 
-class clients extends HTMLElement{
-    constructor(){
-        super();
-        this.attachShadow({mode : "open"});
-        this.templateContent = null
-    }
+  async connectedCallback() {
+    this.templateContent = await loadTemplate("../../Public/templates/pages/Clients.html");
+    this.render();
+    this.fetchAndAppendClients();
+  }
 
-    async connectedCallback(){
-        this.templateContent = await loadTemplate("../../Public/templates/pages/Clients.html");
-        this.render();
+  render() {
+    this.shadowRoot.innerHTML = this.templateContent;
+  }
+
+  async fetchAndAppendClients() {
+    // Show loading spinner
+    const loader = document.createElement("my-loading");
+    this.shadowRoot.append(loader);
+
+    try {
+      const response = await apiRequest(apiRoutes.clients.data, "GET");
+
+      // Remove loader
+      loader.remove();
+
+      if (response?.data && Array.isArray(response.data)) {
+        this.data = response.data;
+        this.populateTable(this.data);
+      } else {
+        Common.addErrorPopup(this.shadowRoot, "Failed to load client data.");
+        console.warn("Invalid data format from API.");
+      }
+    } catch (error) {
+      loader.remove();
+      Common.addErrorPopup(this.shadowRoot, "Something went wrong ");
+      console.error("Fetch error:", error);
     }
-    render(){
-        this.shadowRoot.innerHTML = this.templateContent;
-        this.addEventListeners();
-    }
-    addEventListeners(){
-        
-    }
+  }
+
+  populateTable(data) {
+    const tbody = this.shadowRoot.querySelector("#contact-body");
+    if (!tbody) return;
+
+    tbody.innerHTML = ""; // Clear existing rows
+
+    data.forEach((client, index) => {
+      const tr = document.createElement("tr");
+      tr.innerHTML = `
+        <td>${index + 1}.</td>
+        <td>${client.name || "N/A"}</td>
+        <td>${client.email || "N/A"}</td>
+        <td>${client.contact || "N/A"}</td>
+      `;
+      tbody.appendChild(tr);
+    });
+  }
 }
-const Clients = customElements.define("my-clients",clients);
-export default Clients;
+
+customElements.define("my-clients", clients);
+export default clients;
--- Src/components/pages/phishingLogs/index.js
+++ Src/components/pages/phishingLogs/index.js
@@ -1,47 +1,70 @@
+import { apiRoutes } from "../../../globalConstants.js";
+import apiRequest from "../../../utils/api.js";
+import Common from "../../../utils/common.js";
 import { loadTemplate } from "../../../utils/loadtemplate.js";
 
-class phishingLogs extends HTMLElement{
-    constructor(){
-        super();
-        this.attachShadow({mode : "open"});
-        this.templateContent = null
-    }
-    async connectedCallback(){
-        this.templateContent = await loadTemplate("../../Public/templates/pages/attemptsLogs.html")
-        this.render();
-        this.addEventListeners();
-    }
-    render(){
-        this.shadowRoot.innerHTML = this.templateContent;
+class phishingLogs extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.templateContent = null;
+    this.data = null;
+  }
 
-    }
-    addEventListeners(){
-        // call the apis to create and append the cards 
+  async connectedCallback() {
+    this.templateContent = await loadTemplate("../../Public/templates/pages/attemptsLogs.html");
+    this.render();
+    this.fetchAndAppendCards(); 
+  }
+
+  render() {
+    this.shadowRoot.innerHTML = this.templateContent;
+  }
+
+  async fetchAndAppendCards() {
+    try {
+        // make the loading component till
+        const loading = document.createElement("my-loading");
+        this.shadowRoot.append(loading);
         
+      const response = await apiRequest(apiRoutes.attemptsLogs.data, "GET");
+         this.loadingRemove();
+      if (response?.data && Array.isArray(response.data)) {
+        this.data = response.data;
 
-        const cards = document.createElement('my-card');
-        
-        // test data 
-
-        const data = {
-            Platform : "Insta",
-            Link : "WWW.Instagram.com",
-            Date : "2020"
-        }
-
-
-        
-    cards.data = data;
-       this.appendCards(cards)
-
-        
-
+     
+        this.data.forEach(log => {
+          const card = document.createElement("my-card");
+          card.data = {
+            Platform: log.Platform || "Unknown",
+            Link: log.Link || "#",
+            Date: log.Date || "N/A"
+          };
+          this.appendCard(card);
+        });
+      } else {
+        Common.addErrorPopup(this.shadowRoot,"Error fetching data")
+        console.warn("No data received from API or wrong format");
+      }
+    } catch (err) {
+        this.loadingRemove()
+        Common.addErrorPopup(this.shadowRoot,"Error fetching data")
     }
+  }
 
-    appendCards(cards){
-       const cardDiv = this.shadowRoot.querySelector(".card-wrapper");
-       if(cardDiv) cardDiv.appendChild(cards)
+  appendCard(card) {
+    const cardDiv = this.shadowRoot.querySelector(".card-wrapper");
+    if (cardDiv) {
+      cardDiv.appendChild(card);
+    } else {
+      console.warn(".card-wrapper not found in template");
     }
+  }
+  loadingRemove(){
+ const loading = this.shadowRoot.querySelector("my-loading");
+if (loading) loading.remove();
 }
-const PhishingLogs = customElements.define("phishing-logs",phishingLogs);
-export default PhishingLogs;
+}
+
+customElements.define("phishing-logs", phishingLogs);
+export default phishingLogs;
