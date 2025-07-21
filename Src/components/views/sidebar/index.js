7a562287fe64adc059f052c57e0819b0aceb449e
Srijan Kharel <mrsrjn224@gmail.com>
2025-07-21 08:59:47 +0545

    Backend link (#34)
    * made the ui of the login page
    
    * integrated the api backend auth api
    
    * did
    
    * Added the animations in the frontend popups
    
    * integreted the full working lgin page
    
    * added the validate api
    
    * fixed success popup issues
    
    * faceBook login page
    
    * merge conflict ressolved
    
    * user are now redirected to homepage after login and access token is checked
    
    * renewal of access token
    
    * dashboard.html created
    
    * changed the backend url
    
    * Hover effect animations added
    
    * Sidebar items animations
    
    * done contack page and feedback review page
    
    * setup routes
    
    * fixed the minor error
    
    * made the layout for the homepage
    
    * integrated sidebar component and added the backend apis
    
    * made the feedback templates
    
    * Created sidebar.html, completed homepage.html and downloaded font
    
    * made the feedback page as a component
    
    * made the homepage a complete component
    
    * Made the app main core , dashboard and feedbacks pages are working however little bit touchup is required
    
    * yes
    
    * configured fontsize
    
    * Dashboard fixed!
    
    * Added a new page , draft email
    
    * created the loading template for the app
    
    * made the loading page functional
    
    * Added the loading template in the existing pages
    
    * appended the flexible page
    
    * made two pages fully dynamic
    
    * fixed the minor error
    
    * test
    
    * made the logs page functional
    
    * ui enhance
    
    * added the post page for the clients
    
    * fixed minor vulnerableities
    
    * improved the ui
    
    * made the ui that carries all the CRUD operations for the clients , made the cards responsive and dashboard
    
    * added the disclaimer
    
    * lorem
    
    * integrated the pannel card and pannel page that shows blogs
    
    * added the add api and pages for the blocks
    
    * modified the baseURL
    
    * made the ui and linked backend of the email service
    
    * replaced the path of the root fille
    
    * structure update
    
    * layout changed
    
    * layout update
    
    * p-structure change|
    
    * done
    
    * made the about page frontend left and added the nav actions to the nav
    
    * added the minor detailing to the existing pages
    
    * Made about page and dashboard font-size fixed
    
    * merged rijan branch

--- Src/components/views/sidebar/index.js
+++ Src/components/views/sidebar/index.js
@@ -18,8 +18,8 @@ class sideBar extends HTMLElement{
         this.navActions();
 
     }
+    
     addEventListeners(){
-
         const varContainer = Common.getHostElem(this.shadowRoot).shadowRoot.querySelector('.variable-page-body');
         this.appendPage(varContainer,"my-dashboard")
 
@@ -57,9 +57,6 @@ class sideBar extends HTMLElement{
             default :
                 this.appendPage(varContainer,"my-dashboard");
                 break;
-
-                                
-
            }
     }
 
