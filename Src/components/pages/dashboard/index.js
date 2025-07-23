3147967b4942a97ce31395cf39618e13e683ae80
Rijan Gautam <rijangautam07@gmail.com>
2025-07-23 19:25:58 +0545

    mock data added for the line graph in dashboard

--- Src/components/pages/dashboard/index.js
+++ Src/components/pages/dashboard/index.js
@@ -2,68 +2,73 @@ import { apiRoutes } from "../../../globalConstants.js";
 import apiRequest from "../../../utils/api.js";
 import { loadTemplate } from "../../../utils/loadtemplate.js";
 
-class Dashboard extends HTMLElement{
-    constructor(){
+class Dashboard extends HTMLElement {
+    constructor() {
         super();
-        this.attachShadow({mode : "open"});
+        this.attachShadow({ mode: "open" });
         this.templateContent = null
     }
 
-    async connectedCallback(){
+    async connectedCallback() {
         this.templateContent = await loadTemplate("../../Public/templates/pages/dashboard.html");
         this.render();
         this.addChart();
     }
-    render(){
+    render() {
         this.shadowRoot.innerHTML = this.templateContent;
         this.addEventListeners();
     }
-    addEventListeners(){
-        
+    addEventListeners() {
+
     }
-    addChart(){
+    addChart() {
+        const StatsMockData = {
+            labels: [
+                "Jul 1", "Jul 2", "Jul 3", "Jul 4", "Jul 5",
+                "Jul 6", "Jul 7", "Jul 8", "Jul 9", "Jul 10"
+            ],
+            // values: [25, 80, 75, 20, 95, 30, 45, 65, 30, 85]
+            values: [4 ,5 ,4 ,4 ,4 ,4 ,9 ,4 ,4 ,4 ,4 ,5,4 ,4 ,4]
+        }
         // selecting the chart from the shadowdom
-         const ctx = this.shadowRoot.querySelector('.mygraph');
+        const ctx = this.shadowRoot.querySelector('.mygraph');
         //  accessing as a global var 
-         const Chart = window.Chart;
-         
+        const Chart = window.Chart;
+
         new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: Array(20).fill('Date'),
-            datasets: [{
-                label: '',
-                data: [
-                    25, 80, 75, 20, 95, 30, 45, 65, 30, 85,
-                    50, 100, 95, 100, 85, 75, 20, 90, 60, 30
-                ],
-                borderColor: '#6c3cff',
-                backgroundColor: '#6c3cff',
-                pointRadius: 4,
-                pointHoverRadius: 5,
-                fill: false,
-                tension: 0.2,
-                borderWidth: 2
-            }]
-        },
-        options: {
-            responsive: true,
-            maintainAspectRatio: false,
-            devicePixelRatio: 2,
-            plugins: {
-                legend: {
-                    display: false
-                }
+            type: 'line',
+            data: {
+                labels: StatsMockData.labels,
+                datasets: [{
+                    label: '',
+                    data:StatsMockData.values,
+                    borderColor: '#6c3cff',
+                    backgroundColor: '#6c3cff',
+                    pointRadius: 4,
+                    pointHoverRadius: 5,
+                    fill: false,
+                    tension: 0.2,
+                    borderWidth: 2
+                }]
             },
-            scales: {
-                y: {
-                    beginAtZero: true,
-                    suggestedMax: 125
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                devicePixelRatio: 2,
+                plugins: {
+                    legend: {
+                        display: false
+                    }
+                },
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                        suggestedMax: 25
+                    }
                 }
             }
-        }
-    });
+        });
     }
 }
-const dashboard = customElements.define("my-dashboard",Dashboard);
+const dashboard = customElements.define("my-dashboard", Dashboard);
 export default dashboard;
