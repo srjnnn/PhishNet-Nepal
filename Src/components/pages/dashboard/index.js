b6f01ed3ab06c7f869035ea4514089c4d4d39a99
Srijan Kharel <mrsrjn224@gmail.com>
2025-07-27 12:52:43 +0545

    Backend link (#39)

--- Src/components/pages/dashboard/index.js
+++ Src/components/pages/dashboard/index.js
@@ -1,69 +1,92 @@
 import { apiRoutes } from "../../../globalConstants.js";
 import apiRequest from "../../../utils/api.js";
+import Common from "../../../utils/common.js";
 import { loadTemplate } from "../../../utils/loadtemplate.js";
 
 class Dashboard extends HTMLElement{
     constructor(){
         super();
-        this.attachShadow({mode : "open"});
+        this.attachShadow({ mode: "open" });
         this.templateContent = null
-    }
+        this.labels = null;
+        this.values = null;
+    }; 
 
-    async connectedCallback(){
+    async connectedCallback() {
         this.templateContent = await loadTemplate("../../Public/templates/pages/dashboard.html");
         this.render();
-        this.addChart();
-    }
-    render(){
+        this.CircularGraph(); 
+        this.getData();
+
+    }; 
+    render() {
         this.shadowRoot.innerHTML = this.templateContent;
         this.addEventListeners();
+    }; 
+    addEventListeners() {
+
+    }; 
+    CircularGraph(){
+        const progress_container = this.shadowRoot.querySelector('.outercircle'); 
+        const progress_text = this.shadowRoot.querySelector('.circular-graph-data-text')
+        const progress_data = {
+            people_awared : Common.randomNumber()
+        }
+        progress_container.style.background = `conic-gradient(#602BF8 0% ${progress_data.people_awared}%, #e5e7eb 0%)`; 
+        progress_text.innerHTML = `${progress_data.people_awared}%`; 
+    }; 
+
+    getData(){
+        const loading = Common.loadingTemplate("Fetching Stats.................");
+        this.shadowRoot.append(loading);
+         apiRequest(apiRoutes.dashboard.getStatsData,"GET")
+        .then((response)=>{
+            this.labels = response.data[0].data.labels;
+            this.values = response.data[0].data.values;
+            this.addChart();
+            loading.remove();
+        })
     }
-    addEventListeners(){
-        
-    }
-    addChart(){
-        // selecting the chart from the shadowdom
-         const ctx = this.shadowRoot.querySelector('.mygraph');
-        //  accessing as a global var 
-         const Chart = window.Chart;
-         
+    addChart() {
+        const ctx = this.shadowRoot.querySelector('.mygraph');
+
+        const Chart = window.Chart;
+
         new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: Array(20).fill('Date'),
-            datasets: [{
-                label: '',
-                data: [
-                    25, 80, 75, 20, 95, 30, 45, 65, 30, 85,
-                    50, 100, 95, 100, 85, 75, 20, 90, 60, 30
-                ],
-                borderColor: '#6c3cff',
-                backgroundColor: '#6c3cff',
-                pointRadius: 4,
-                pointHoverRadius: 5,
-                fill: false,
-                tension: 0.2,
-                borderWidth: 2
-            }]
-        },
-        options: {
-            responsive: true,
-            maintainAspectRatio: false,
-            devicePixelRatio: 2,
-            plugins: {
-                legend: {
-                    display: false
-                }
+            type: 'line',
+            data: {
+                labels:this.labels ,
+                datasets: [{
+                    label: '',
+                    data:this.values,
+                    borderColor: '#6c3cff',
+                    backgroundColor: '#6c3cff',
+                    pointRadius: 4,
+                    pointHoverRadius: 5,
+                    fill: false,
+                    tension: 0.2,
+                    borderWidth: 2
+                }]
+
             },
-            scales: {
-                y: {
-                    beginAtZero: true,
-                    suggestedMax: 125
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                devicePixelRatio: 2,
+                plugins: {
+                    legend: {
+                        display: false
+                    }
+                },
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                        suggestedMax: 25
+                    }
                 }
             }
-        }
-    });
+        });
     }
 }
-const dashboard = customElements.define("my-dashboard",Dashboard);
+const dashboard = customElements.define("my-dashboard", Dashboard);
 export default dashboard;
